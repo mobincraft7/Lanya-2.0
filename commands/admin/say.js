91c388c1a83db3463ea68d134d8db0bdb58bbb01
mobincraft7 <mobinriazi2024@gmail.com>
2026-01-05 23:16:21 +0330

    Update say.js

--- commands/admin/say.js
+++ commands/admin/say.js
@@ -12,7 +12,7 @@ module.exports = {
 
     const modal = new ModalBuilder()
       .setCustomId('sayModal')
-      .setTitle('Nasl-1 Say System');
+      .setTitle('Aluxit System');
 
     const messageInput = new TextInputBuilder()
       .setCustomId('message')
